95b65d50d81f30eb258c5ea06110d0e2f1dccf7d
karithomas <kari.k.thomas8@gmail.com>
2019-08-24 16:37:14 -0400

    Final homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
-var letters = "abcdefghijklmnopqrstuvwxyz".split("");
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
 var guesses = 9;
 var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 var userGuessSoFar = [];
 var winPoints = document.getElementById("wins");
-var guessesLeft = document.getElementById("guessLeft");
 var losePoints = document.getElementById("losses");
-var guessSoFar = document.getElementById("guess-so-far");
+var guessesLeft = document.getElementById("guessesLeft");
+var guessSoFar = document.getElementById("userGuesses");
 
 function updateDisplays() {
   winPoints.textContent = wins;
@@ -26,7 +26,8 @@ document.onkeyup = function(event) {
     guesses = 9
     userGuessSoFar = [];
     computerGuess = letters[Math.floor(Math.random() * letters.length)];
-  } else {
+  } 
+  else {
     guesses--;
     if (guesses == 0) {
       losses++;
@@ -38,5 +39,4 @@ document.onkeyup = function(event) {
   updateDisplays();
 
 }
-
 updateDisplays();
